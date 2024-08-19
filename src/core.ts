1d53859e7695e426713f55daa5f89629481ab801
Rom Grk <romgrk.cc@gmail.com>
2024-08-19 16:39:42 -0400

    initial commit

---
+++ src/core.ts
@@ -0,0 +1,59 @@
+import { cast, get, set } from './bit';
+
+export type Color = number;
+
+export const OFFSET_R = 24;
+export const OFFSET_G = 16;
+export const OFFSET_B =  8;
+export const OFFSET_A =  0;
+
+export function newColor(r: number, g: number, b: number, a: number) {
+  // prettier-ignore
+  return (
+    (r << OFFSET_R) +
+    (g << OFFSET_G) +
+    (b << OFFSET_B) +
+    (a << OFFSET_A)
+  );
+}
+
+export function from(hex: number) {
+  return newColor(
+    get(hex, OFFSET_R),
+    get(hex, OFFSET_G),
+    get(hex, OFFSET_B),
+    get(hex, OFFSET_A),
+  );
+}
+
+export function toNumber(color: Color) {
+  return cast(color);
+}
+
+export function getRed(c: Color) { return get(c, OFFSET_R); }
+export function getGreen(c: Color) { return get(c, OFFSET_G); }
+export function getBlue(c: Color) { return get(c, OFFSET_B); }
+export function getAlpha(c: Color) { return get(c, OFFSET_A); }
+
+export function setRed(c: Color, value: number) { return set(c, OFFSET_R, value); }
+export function setGreen(c: Color, value: number) { return set(c, OFFSET_G, value); }
+export function setBlue(c: Color, value: number) { return set(c, OFFSET_B, value); }
+export function setAlpha(c: Color, value: number) { return set(c, OFFSET_A, value); }
+
+/**
+ * Map 8-bits value to its hexadecimal representation
+ * ['00', '01', '02', ..., 'fe', 'ff']
+ */
+const FORMAT_HEX =
+  Array.from({ length: 256 })
+    .map((_, byte) => byte.toString(16).padStart(2, '0'))
+
+export function format(color: Color): string {
+  /*
+   * Implementing this as `cast(color).toString(16).padStart(8, '0')` would be simpler,
+   * but `cast()` makes the color a non-Smi value on V8, as well as allocates more strings
+   * and makes more function calls.
+   * This version is about 4 times faster than the simple one.
+   */
+  return '#' + FORMAT_HEX[getRed(color)] + FORMAT_HEX[getGreen(color)] + FORMAT_HEX[getBlue(color)] + FORMAT_HEX[getAlpha(color)];
+}
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+'use client';
+export * from './core';
+export * from './parse';
+export * from './functions';
